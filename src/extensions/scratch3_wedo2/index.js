8c4259da93a7c139f96ac45a7ab203a9b9b6b289
Shinichiro Oba <ooba@bricklife.com>
2018-09-26 13:31:15 +0100

    Support LED Light

--- src/extensions/scratch3_wedo2/index.js
+++ src/extensions/scratch3_wedo2/index.js
@@ -45,6 +45,7 @@ const BLESendRateMax = 20;
 const WeDo2Types = {
     MOTOR: 1,
     TRAIN_MOTOR: 2,
+    LED_LIGHT: 8,
     PIEZO: 22,
     LED: 23,
     TILT: 34,
@@ -641,7 +642,7 @@ class WeDo2 {
         this._ports[connectID - 1] = type;
 
         // Register motor
-        if (type === WeDo2Types.MOTOR || type === WeDo2Types.TRAIN_MOTOR) {
+        if (type === WeDo2Types.MOTOR || type === WeDo2Types.TRAIN_MOTOR || type === WeDo2Types.LED_LIGHT) {
             this._motors[connectID - 1] = new WeDo2Motor(this, connectID - 1);
         } else {
             // Register tilt or distance sensor
